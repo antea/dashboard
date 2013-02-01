67f118e27e1af52bf09b11490690600c7e336df4
Matteo Salvà <salv.matteo@gmail.com>
2013-02-01 14:32:08 +0100

    Addeed animation.js

---
+++ public_html/js/animations.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var INTERVAL = 10000;
+var TIMEOUT;
+getGit();
+getJenkins();
+$('.carousel').carousel({
+    interval: INTERVAL
+});
+$("#progress").css("-webkit-animation-duration", (INTERVAL / 1000) - 0.8 + "s");
+$('.carousel').mouseenter(function() {
+    window.clearTimeout(TIMEOUT);
+    $("#progress").css("-webkit-animation-play-state", "paused");
+});
+$('.carousel').mouseleave(function() {
+    TIMEOUT = setTimeout("restartBar()", valutaPercentuale());
+});
+$(".carousel").on("slide", function() {
+    $("#progress").attr("id", "progres");
+});
+$(".carousel").on("slid", function() {
+    var attr = $(".carousel .active").attr("id") + "Li";
+    $("#navbar > li").removeClass();
+    $("#" + attr).addClass("active");
+    $("#progres").attr("id", "progress");
+});
+var valutaPercentuale = function() {
+    var width = $('#progress').width();
+    var widthMax = $('#progContainer').width();
+    return INTERVAL * ((widthMax - width) / widthMax);
+};
+var restartBar = function() {
+    $("#progress").css("-webkit-animation-play-state", "running");
+};
+
