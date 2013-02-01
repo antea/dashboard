604541e8d31266dc77da49b4e0c769d0ae3a18b0
Matteo Bonetto <bonetto.matteo@anteash.com>
2013-02-01 12:11:27 +0100

    Last changes to git and jenkins javascript

--- public_html/js/git.js
+++ public_html/js/git.js
@@ -3,53 +3,60 @@
  * and open the template in the editor.
  */
 
-$.get(gitUrl, function(data) {
-    var $xml = $(data);
-    $xml.find("item").each(function(index) {
-        var $this = $(this);
-        var item = {
-            title: $this.find("title").text(),
-            pubDate: new Date($this.find("pubDate").text()),
-            author: $this.find("author").text().split(" <")[0]
-        };
-        $("#gitDiv").append("<blockquote><p>" + item.title + "</p>" + "<small>" +
-                item.author + " il " + item.pubDate.toLocaleString() + ".</small></blockquote>");
-        if (index === 4) {
-            return false;
-        }
-    });
-});
-
-$.get(gitWebUrl, function(result) {
-    var $data = $(result);
-    var found = [];
-    var i = 0;
-    $data.find("tr").each(function(index) {
-        var $this = $(this);
-        $this.find("td").each(function(index) {
+getGit = function() {
+    $.get(gitUrl, function(data) {
+        var $xml = $(data);
+        $xml.find("item").each(function(index) {
             var $this = $(this);
-            if (index === 0) {
-                found[i] = $this;
-                found[i].text(found[i].text().replace("months","mesi"));
-                found[i].text(found[i].text().replace("month","mese"));
-                found[i].text(found[i].text().replace("weeks","settimane"));
-                found[i].text(found[i].text().replace("week","settimana"));
-                found[i].text(found[i].text().replace("hours","ore"));
-                found[i].text(found[i].text().replace("hour","ora"));
-                found[i].text(found[i].text().replace("ago","fa"));
-            }
-            else {
-                found[i + 1] = $this;
-                i = i + 2;
+            var item = {
+                title: $this.find("title").text(),
+                pubDate: new Date($this.find("pubDate").text()),
+                author: $this.find("author").text().split(" <")[0]
+            };
+            $("#commitDiv").append("<blockquote><p>" + item.title + "</p>" + "<p><small>" +
+                    item.author + " il " + item.pubDate.toLocaleString() + ".</p></small></blockquote>");
+            if (index === 4) {
                 return false;
             }
         });
-        if (index === 4) {
-            return false;
+    });
+
+    $.get(gitWebUrl, function(result) {
+        var $data = $(result);
+        
+        function branch() {
+            this.name;
+            this.date;
+        }
+        
+        var found = [];
+        //var i = 0;
+        $data.find("tr").each(function(i) {
+            found[i] = new branch();
+            $this = $(this);
+            $this.find("td").each(function(j) {
+                var $this = $(this);
+                if (j === 0) {
+                    found[i].date = $this;
+                    found[i].date.text(found[i].date.text().replace("months", "mesi"));
+                    found[i].date.text(found[i].date.text().replace("month", "mese"));
+                    found[i].date.text(found[i].date.text().replace("weeks", "settimane"));
+                    found[i].date.text(found[i].date.text().replace("week", "settimana"));
+                    found[i].date.text(found[i].date.text().replace("hours", "ore"));
+                    found[i].date.text(found[i].date.text().replace("hour", "ora"));
+                    found[i].date.text(found[i].date.text().replace("ago", "fa"));
+                }
+                else if (j === 1){
+                    found[i].name = $this;
+                    return false;
+                }
+            });
+            if (i === 4) {
+                return false;
+            }
+        });
+        for (i = 0; i < found.length; i++) {
+            $("#branchDiv").append("<blockquote><p>" + found[i].name.text() + "</p><p><small>" + found[i].date.text() + "</p></small></blockquote>");
         }
     });
-    for (i = 0; i < found.length; i += 2) {
-        $("#branchDiv").append("<blockquote><p>" + found[i + 1].text() + "</p><small>" + found[i].text() + "</small></blockquote>");
-    }
-});
-
+};
--- public_html/js/jenkins.js
+++ public_html/js/jenkins.js
@@ -2,59 +2,62 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-var found = [];
 
-function build() {
-    this.name;
-    this.color;
-    this.date;
-    this.state;
-}
+getJenkins = function() {
+    var found = [];
 
-$.getJSON(jenkinsUrl, function(data) {
-    var obj = data;
-    var j = 0;
-    for (i = 0; i < obj.jobs.length; i++) {
-        var jobColor = obj.jobs[i].color;
-        found[i] = new build();
-        found[i].name = obj.jobs[i].name;
-        switch (jobColor) {
-            case "blue" :
-                found[i].state = "fermo";
-                found[i].color = "alert alert-info";
-                break;
-            case "blue_anime" :
-                found[i].state = "build in corso";
-                found[i].color = "alert alert-info";
-                break;
-            case "red":
-                found[i].state = "fermo";
-                found[i].color = "alert alert-error";
-                break;
-            case "red_anime":
-                found[i].state = "build in corso";
-                found[i].color = "alert alert-error";
-                break;
-            case "yellow":
-                found[i].state = "fermo";
-                found[i].color = "alert";
-                break;
-            case "yellow_anime":
-                found[i].state = "build in corso";
-                found[i].color = "alert";
-                break;
-        }
+    function build() {
+        this.name;
+        this.color;
+        this.date;
+        this.state;
     }
-    $.get(jenkinsUrlDate, function(xml) {
-        var $xml = $(xml);
-        $xml.find("entry").each(function(index) {
-            $this = $(this);
-            var date = new Date($this.find("published").text());
-            found[index].date = date;
-        });
-        for (i = 0; i < found.length; i++) {
-            $("#jenkinsDiv").append("<tr class=\"" + found[i].color + "\"><td><strong>" + found[i].name + "</strong></td><td>" + found[i].date.toLocaleString() +
-                    "</td><td>" + found[i].state + "</td></tr>");
+
+    $.getJSON(jenkinsUrl, function(data) {
+        var obj = data;
+        var j = 0;
+        for (i = 0; i < obj.jobs.length; i++) {
+            var jobColor = obj.jobs[i].color;
+            found[i] = new build();
+            found[i].name = obj.jobs[i].name;
+            switch (jobColor) {
+                case "blue" :
+                    found[i].state = "fermo";
+                    found[i].color = "alert alert-info";
+                    break;
+                case "blue_anime" :
+                    found[i].state = "build in corso";
+                    found[i].color = "alert alert-info";
+                    break;
+                case "red":
+                    found[i].state = "fermo";
+                    found[i].color = "alert alert-error";
+                    break;
+                case "red_anime":
+                    found[i].state = "build in corso";
+                    found[i].color = "alert alert-error";
+                    break;
+                case "yellow":
+                    found[i].state = "fermo";
+                    found[i].color = "alert";
+                    break;
+                case "yellow_anime":
+                    found[i].state = "build in corso";
+                    found[i].color = "alert";
+                    break;
+            }
         }
+        $.get(jenkinsUrlDate, function(xml) {
+            var $xml = $(xml);
+            $xml.find("entry").each(function(index) {
+                $this = $(this);
+                var date = new Date($this.find("published").text());
+                found[index].date = date;
+            });
+            for (i = 0; i < found.length; i++) {
+                $("#jenkinsTable").append("<tr class=\"" + found[i].color + "\"><td><strong>" + found[i].name + "</strong></td><td>" + found[i].date.toLocaleString() +
+                        "</td><td>" + found[i].state + "</td></tr>");
+            }
+        });
     });
-});
+};
