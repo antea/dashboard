f2325e62fca00a2fe081fc5ba7c57a60432f1a5c
Matteo Bonetto <bonetto.matteo@anteash.com>
2013-02-07 14:56:30 +0100

    Modifications in git.js and jenkins.js

--- public_html/js/jenkins.js
+++ public_html/js/jenkins.js
@@ -3,73 +3,70 @@
  * and open the template in the editor.
  */
 
-var jenkinsCheck;
-
-fetchJenkins = function(data, textStatus) {
-    var found = [];
-
-    function build() {
-        this.name;
-        this.color;
-        this.date;
-        this.state;
-    }
-
-    if (textStatus !== "success") {
-        return;
-    } else {
-        jenkinsCheck = (new Date()).getTime();
-        var obj = data;
-        var j = 0;
-        for (i = 0; i < obj.jobs.length; i++) {
-            var jobColor = obj.jobs[i].color;
-            found[i] = new build();
-            found[i].name = obj.jobs[i].name;
-            switch (jobColor) {
-                case "blue" :
-                    found[i].state = "fermo";
-                    found[i].color = "alert alert-info";
-                    break;
-                case "blue_anime" :
-                    found[i].state = "build in corso";
-                    found[i].color = "alert alert-info";
-                    break;
-                case "red":
-                    found[i].state = "fermo";
-                    found[i].color = "alert alert-error";
-                    break;
-                case "red_anime":
-                    found[i].state = "build in corso";
-                    found[i].color = "alert alert-error";
-                    break;
-                case "yellow":
-                    found[i].state = "fermo";
-                    found[i].color = "alert";
-                    break;
-                case "yellow_anime":
-                    found[i].state = "build in corso";
-                    found[i].color = "alert";
-                    break;
-            }
-        }
-        $.get(jenkinsUrlDate, function(xml) {
-            var $xml = $(xml);
-            $xml.find("entry").each(function(index) {
-                $this = $(this);
-                var date = new Date($this.find("published").text());
-                found[index].date = date;
-            });
-            $("#jenkinsTable").empty();
-            for (i = 0; i < found.length; i++) {
-                $("#jenkinsTable").append("<tr class=\"" + found[i].color + "\"><td>" + found[i].name + "</td><td>" + found[i].date.toLocaleString() +
-                        "</td><td>" + found[i].state + "</td></tr>");
-            }
-        });
-    }
-};
+var jenkinsCheck = (new Date()).getTime();
+var found = [];
+function build() {
+    this.name;
+    this.color;
+    this.date;
+    this.state;
+}
 
 getJenkins = function(url) {
     $.getJSON(url, function(data, textStatus) {
-        fetchJenkins(data, textStatus);
+        if (textStatus !== "success") {
+            return;
+        } else {
+            jenkinsCheck = (new Date()).getTime();
+            var obj = data;
+            var j = 0;
+            for (i = 0; i < obj.jobs.length; i++) {
+                var jobColor = obj.jobs[i].color;
+                found[i] = new build();
+                found[i].name = obj.jobs[i].name;
+                switch (jobColor) {
+                    case "blue" :
+                        found[i].state = "fermo";
+                        found[i].color = "alert alert-info";
+                        break;
+                    case "blue_anime" :
+                        found[i].state = "build in corso";
+                        found[i].color = "alert alert-info";
+                        break;
+                    case "red":
+                        found[i].state = "fermo";
+                        found[i].color = "alert alert-error";
+                        break;
+                    case "red_anime":
+                        found[i].state = "build in corso";
+                        found[i].color = "alert alert-error";
+                        break;
+                    case "yellow":
+                        found[i].state = "fermo";
+                        found[i].color = "alert";
+                        break;
+                    case "yellow_anime":
+                        found[i].state = "build in corso";
+                        found[i].color = "alert";
+                        break;
+                }
+            }
+        }
+    });
+};
+
+getJenkinsDate = function(url) {
+    $.get(url, function(xml) {
+        var $xml = $(xml);
+        $xml.find("entry").each(function(index) {
+            $this = $(this);
+            var date = new Date($this.find("published").text());
+            found[index].date = date;
+        });
+        $("#jenkinsTable").empty();
+        for (i = 0; i < found.length; i++) {
+            $("#jenkinsTable").append("<tr class=\"" + found[i].color + "\"><td>" + found[i].name + "</td><td>" + found[i].date.toLocaleString() +
+                    "</td><td>" + found[i].state + "</td></tr>");
+        }
     });
 };
