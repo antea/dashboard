b6e017962995474e33fcac6accc3897ce4fe35c7
Matteo Bonetto <bonetto.matteo@anteash.com>
2013-02-13 12:34:06 +0100

    Refactoring of git.js and jenkins.js

--- public_html/js/git.js
+++ public_html/js/git.js
@@ -6,78 +6,86 @@
 var gitCheck = (new Date()).getTime();
 
 getGit = function(urlCommit, urlBranch) {
-    fetchCommit(urlCommit);
-    fetchBranch(urlBranch);
+    fetch(urlBranch, parseBranch, printBranch);
+    fetch(urlCommit, parseCommit, printCommit);
+};
 
-    function fetchCommit(url) {
-        $.get(url, function(data, textStatus) {
-            if (textStatus !== "success") {
-                return;
-            } else {
-                gitCheck = (new Date()).getTime();
-                var $xml = $(data);
-                $("#commitDiv").empty();
-                $("#commitDiv").append("<h4>Commit più recenti</h4>");
-                $xml.find("item").each(function(index) {
-                    var $this = $(this);
-                    var item = {
-                        title: $this.find("title").text(),
-                        pubDate: new Date($this.find("pubDate").text()),
-                        author: $this.find("author").text().split(" <")[0]
-                    };
-                    $("#commitDiv").append("<blockquote><p>" + item.title + "</p>" + "<p><small>" +
-                            item.author + " il " + item.pubDate.toLocaleString() + ".</p></small></blockquote>");
-                    if (index === 4) {
-                        return false;
-                    }
-                });
-            }
-        });
+var printBranch = function(parsedData) {
+    $("#branchDiv").empty();
+    $("#branchDiv").append("<h4>Branch più recenti</h4>");
+    for (i = 0; i < parsedData.length; i++) {
+        $("#branchDiv").append("<blockquote><p>" + parsedData[i].name.text() + "</p><p><small>" + parsedData[i].date.text() + "</p></small></blockquote>");
     }
+};
 
-    function fetchBranch(url) {
-        $.get(url, function(data, status) {
-            if (status !== "success") {
+var parseBranch = function(data, printFunction) {
+    var found = [];
+    function branch() {
+        this.name;
+        this.date;
+    }
+    data.find("tr").each(function(i) {
+        found[i] = new branch();
+        $this = $(this);
+        $this.find("td").each(function(j) {
+            var $this = $(this);
+            if (j === 0) {
+                found[i].date = $this;
+                found[i].date.text(found[i].date.text().replace("months", "mesi"));
+                found[i].date.text(found[i].date.text().replace("month", "mese"));
+                found[i].date.text(found[i].date.text().replace("weeks", "settimane"));
+                found[i].date.text(found[i].date.text().replace("week", "settimana"));
+                found[i].date.text(found[i].date.text().replace("hours", "ore"));
+                found[i].date.text(found[i].date.text().replace("hour", "ora"));
+                found[i].date.text(found[i].date.text().replace("ago", "fa"));
+            }
+            else if (j === 1) {
+                found[i].name = $this;
                 return false;
-            } else {
-                var $data = $(data);
-                function branch() {
-                    this.name;
-                    this.date;
-                }
-
-                var found = [];
-                $data.find("tr").each(function(i) {
-                    found[i] = new branch();
-                    $this = $(this);
-                    $this.find("td").each(function(j) {
-                        var $this = $(this);
-                        if (j === 0) {
-                            found[i].date = $this;
-                            found[i].date.text(found[i].date.text().replace("months", "mesi"));
-                            found[i].date.text(found[i].date.text().replace("month", "mese"));
-                            found[i].date.text(found[i].date.text().replace("weeks", "settimane"));
-                            found[i].date.text(found[i].date.text().replace("week", "settimana"));
-                            found[i].date.text(found[i].date.text().replace("hours", "ore"));
-                            found[i].date.text(found[i].date.text().replace("hour", "ora"));
-                            found[i].date.text(found[i].date.text().replace("ago", "fa"));
-                        }
-                        else if (j === 1) {
-                            found[i].name = $this;
-                            return false;
-                        }
-                    });
-                    if (i === 4) {
-                        return false;
-                    }
-                });
-                $("#branchDiv").empty();
-                $("#branchDiv").append("<h4>Branch più recenti</h4>");
-                for (i = 0; i < found.length; i++) {
-                    $("#branchDiv").append("<blockquote><p>" + found[i].name.text() + "</p><p><small>" + found[i].date.text() + "</p></small></blockquote>");
-                }
-                return true;
             }
         });
+        if (i === 4) {
+            printFunction(found);
+            return false;
+        }
+    });
+};
+
+var printCommit = function(parsedData) {
+    $("#commitDiv").empty();
+    $("#commitDiv").append("<h4>Commit più recenti</h4>");
+    for (i = 0; i < parsedData.length; i++) {
+        $("#commitDiv").append("<blockquote><p>" + parsedData[i].title + "</p>" + "<p><small>" +
+                parsedData[i].author + " il " + parsedData[i].pubDate.toLocaleString() + ".</p></small></blockquote>");
     }
+};
+
+var parseCommit = function(data, printFunction) {
+    var parsedData = [];
+    data.find("item").each(function(index) {
+        var $this = $(this);
+        var item = {
+            title: $this.find("title").text(),
+            pubDate: new Date($this.find("pubDate").text()),
+            author: $this.find("author").text().split(" <")[0]
+        };
+
+        parsedData[index] = item;
+        if (index === 4) {
+            printFunction(parsedData);
+            return false;
+        }
+    });
+};
+
+var fetch = function(url, parseFunction, printFunction) {
+    $.get(url, function(data, textStatus) {
+        if (textStatus !== "success") {
+            return;
+        } else {
+            gitCheck = (new Date()).getTime();
+            $data = $(data);
+            parseFunction($data, printFunction);
+        }
+    });
 };
--- public_html/js/jenkins.js
+++ public_html/js/jenkins.js
@@ -6,65 +6,74 @@
 var jenkinsCheck = (new Date()).getTime();
 
 getJenkins = function(url, urlDate) {
+    fetchJenkins(url, urlDate, parseJenkins, printJenkins);
+};
+var printJenkins = function(parsedData) {
+    $("#jenkinsTable").empty();
+    for (i = 0; i < parsedData.length; i++) {
+        $("#jenkinsTable").append("<tr class=\"" + parsedData[i].color + "\"><td>" + parsedData[i].name + "</td><td>" + parsedData[i].date.toLocaleString() +
+                "</td><td>" + parsedData[i].state + "</td></tr>");
+    }
+};
+
+var parseJenkins = function(json, xml, printFunction) {
     var found = [];
-    
     function build() {
         this.name;
         this.color;
         this.date;
         this.state;
     }
-    
-    $.getJSON(url, function(data, textStatus) {
+    var obj = json;
+    for (i = 0; i < obj.jobs.length; i++) {
+        var jobColor = obj.jobs[i].color;
+        found[i] = new build();
+        found[i].name = obj.jobs[i].name;
+        switch (jobColor) {
+            case "blue" :
+                found[i].state = "fermo";
+                found[i].color = "alert alert-info";
+                break;
+            case "blue_anime" :
+                found[i].state = "build in corso";
+                found[i].color = "alert alert-info";
+                break;
+            case "red":
+                found[i].state = "fermo";
+                found[i].color = "alert alert-error";
+                break;
+            case "red_anime":
+                found[i].state = "build in corso";
+                found[i].color = "alert alert-error";
+                break;
+            case "yellow":
+                found[i].state = "fermo";
+                found[i].color = "alert";
+                break;
+            case "yellow_anime":
+                found[i].state = "build in corso";
+                found[i].color = "alert";
+                break;
+        }
+    }
+    xml.find("entry").each(function(index) {
+        $this = $(this);
+        var date = new Date($this.find("published").text());
+        found[index].date = date;
+    });
+    printFunction(found);
+};
+
+var fetchJenkins = function(url, urlDate, parseFunction, printFunction) {
+    $.getJSON(url, function(json, textStatus) {
         if (textStatus !== "success") {
             return;
         } else {
             jenkinsCheck = (new Date()).getTime();
-            var obj = data;
-            for (i = 0; i < obj.jobs.length; i++) {
-                var jobColor = obj.jobs[i].color;
-                found[i] = new build();
-                found[i].name = obj.jobs[i].name;
-                switch (jobColor) {
-                    case "blue" :
-                        found[i].state = "fermo";
-                        found[i].color = "alert alert-info";
-                        break;
-                    case "blue_anime" :
-                        found[i].state = "build in corso";
-                        found[i].color = "alert alert-info";
-                        break;
-                    case "red":
-                        found[i].state = "fermo";
-                        found[i].color = "alert alert-error";
-                        break;
-                    case "red_anime":
-                        found[i].state = "build in corso";
-                        found[i].color = "alert alert-error";
-                        break;
-                    case "yellow":
-                        found[i].state = "fermo";
-                        found[i].color = "alert";
-                        break;
-                    case "yellow_anime":
-                        found[i].state = "build in corso";
-                        found[i].color = "alert";
-                        break;
-                }
-            }
-        }
-    });
-    $.get(urlDate, function(xml) {
-        var $xml = $(xml);
-        $xml.find("entry").each(function(index) {
-            $this = $(this);
-            var date = new Date($this.find("published").text());
-            found[index].date = date;
-        });
-        $("#jenkinsTable").empty();
-        for (i = 0; i < found.length; i++) {
-            $("#jenkinsTable").append("<tr class=\"" + found[i].color + "\"><td>" + found[i].name + "</td><td>" + found[i].date.toLocaleString() +
-                    "</td><td>" + found[i].state + "</td></tr>");
+            $.get(urlDate, function(xml) {
+                $xml = $(xml);
+                parseFunction(json, $xml, printFunction);
+            });
         }
     });
 };
