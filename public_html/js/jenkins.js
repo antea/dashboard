8bf603ea35469b3e2d823c29fc592985b73d8729
Matteo Bonetto <bonetto.matteo@anteash.com>
2013-02-18 11:03:56 +0100

    Aggiunti commenti al codice di jenkins.js

--- public_html/js/jenkins.js
+++ public_html/js/jenkins.js
@@ -4,77 +4,74 @@
  */
 
 var jenkinsCheck = (new Date()).getTime();
-var jenkinsAppend = "#jenkinsTable";
 
 getJenkins = function(url, urlDate) {
-    $.getJSON(url, fetchJenkins(urlDate, parseJenkins, printJenkins));
-};
-var printJenkins = function(parsedData) {
-    $(jenkinsAppend).empty();
-    for (i = 0; i < parsedData.length; i++) {
-        $(jenkinsAppend).append("<tr><td>" + parsedData[i].label + "</td><td>" + parsedData[i].name + "</td><td>" + parsedData[i].date.toLocaleString() +
-                "</td><td>" + parsedData[i].state + "</td></tr>");
-    }
-};
+    var jenkinsAppend = "#jenkinsTable"; //Variabile che memorizza la tabella destinata alla stampa dei build
 
-var parseJenkins = function(json, xml, printFunction) {
-    var found = [];
-    function build() {
-        this.name;
-        this.date;
-        this.state;
-        this.label;
-    }
-    var obj = json;
-    for (i = 0; i < obj.jobs.length; i++) {
-        var jobColor = obj.jobs[i].color;
-        found[i] = new build();
-        found[i].name = obj.jobs[i].name;
-        switch (jobColor) {
-            case "blue" :
-                found[i].state = "fermo";
-                found[i].label = " <span class=\"label label-info pull-right\">Stabile</span> ";
-                break;
-            case "blue_anime" :
-                found[i].state = "build in corso";
-                found[i].label = " <span class=\"label label-info pull-right\">Stabile</span> ";
-                break;
-            case "red":
-                found[i].state = "fermo";
-                found[i].label = " <span class=\"label label-important pull-right\">Danneggiata</span> ";
-                break;
-            case "red_anime":
-                found[i].state = "build in corso";
-                found[i].label = " <span class=\"label label-important pull-right\">Danneggiata</span> ";
-                break;
-            case "yellow":
-                found[i].state = "fermo";
-                found[i].label = " <span class=\"label label-warning pull-right\">Instabile</span> ";
-                break;
-            case "yellow_anime":
-                found[i].state = "build in corso";
-                found[i].label = " <span class=\"label label-warning pull-right\">Instabile</span> ";
-                break;
-        }
-    }
-    xml.find("entry").each(function(index) {
-        $this = $(this);
-        var date = new Date($this.find("published").text());
-        found[index].date = date;
-    });
-    printFunction(found);
-};
-
-var fetchJenkins = function(urlDate, parseFunction, printFunction) {
-    return function(json, textStatus) {
-        if (textStatus !== "success") {
-            return textStatus;
-        } else {
-            jenkinsCheck = (new Date()).getTime();
-            $.get(urlDate, function(xml) {
-                $xml = $(xml);
-                parseFunction(json, $xml, printFunction);
-            });
+    var printJenkins = function(parsedData) {
+        $(jenkinsAppend).empty();
+        for (i = 0; i < parsedData.length; i++) {
+            $(jenkinsAppend).append("<tr><td>" + parsedData[i].label + "</td><td>" + parsedData[i].name + "</td><td>" + parsedData[i].date.toLocaleString() +
+                    "</td><td>" + parsedData[i].state + "</td></tr>");
         }
     };
+
+    var parseJenkins = function(json, xml, printFunction) {
+        var found = []; //Array da popolare con i dati trovati, da passare alla funzione di stampa
+        var $json = $(json.jobs);
+        $json.each(function(i) {
+            found[i] = {};
+            found[i].name = this.name;
+            var jobColor = this.color;
+            switch (jobColor) {
+                case "blue" :
+                    found[i].state = "fermo";
+                    found[i].label = " <span class=\"label label-info pull-right\">Stabile</span> ";
+                    break;
+                case "blue_anime" :
+                    found[i].state = "build in corso";
+                    found[i].label = " <span class=\"label label-info pull-right\">Stabile</span> ";
+                    break;
+                case "red":
+                    found[i].state = "fermo";
+                    found[i].label = " <span class=\"label label-important pull-right\">Rotto</span> ";
+                    break;
+                case "red_anime":
+                    found[i].state = "build in corso";
+                    found[i].label = " <span class=\"label label-important pull-right\">Rotto</span> ";
+                    break;
+                case "yellow":
+                    found[i].state = "fermo";
+                    found[i].label = " <span class=\"label label-warning pull-right\">Instabile</span> ";
+                    break;
+                case "yellow_anime":
+                    found[i].state = "build in corso";
+                    found[i].label = " <span class=\"label label-warning pull-right\">Instabile</span> ";
+                    break;
+            }
+        });
+        
+        //Recupera la data di build
+        xml.find("entry").each(function(i) {
+            $this = $(this);
+            var date = new Date($this.find("published").text());
+            found[i].date = date;
+        });
+        printFunction(found);
+    };
+
+    var fetchJenkins = function(urlDate, parseFunction, printFunction) {
+        return function(json, textStatus) {
+            if (textStatus !== "success") {
+                return textStatus;
+            } else {
+                jenkinsCheck = (new Date()).getTime();
+                $.get(urlDate, function(xml) {
+                    $xml = $(xml);
+                    parseFunction(json, $xml, printFunction);
+                });
+            }
+        };
+    };
+    $.getJSON(url, fetchJenkins(urlDate, parseJenkins, printJenkins));
 };
