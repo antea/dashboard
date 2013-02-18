5a7ff52552a498cea04637befd656c63e069a7c9
Matteo Salvà <salv.matteo@gmail.com>
2013-02-18 12:30:39 +0100

    Redmine refactoring

--- public_html/js/redmine.js
+++ public_html/js/redmine.js
@@ -5,41 +5,60 @@
  * you must retrieve the redmineKey from your personal Redmine page.
  */
 
-var statusOngoing = "2";
-var lowPriority = "3";
-var normalPriority = "4";
-var urgentPriority = "6";
-var immediatePriority = "7";
-var redmineTable = "#redmineTable";
+var STATUS_ONGOING = "2";
+var LOW_PRIORITY = "3";
+var NORMAL_PRIORITY = "4";
+var URGENT_PRIORITY = "6";
+var IMMEDIATE_PRIORITY = "7";
+var REDMINE_TABLE = "#redmineTable";
 var redmineCheck = Date.now();
 
+/**
+ * This function makes a request to redmine, than call callbackRedmineRequest.
+ * 
+ */
 function getRedmine() {
     $.getJSON(redmineUrl,
-            {'key': redmineKey, 'status_id': statusOngoing}
+            {'key': redmineKey, 'status_id': STATUS_ONGOING}
     , callbackRedmineRequest(appendRedmineToHtml));
 }
 
+/**
+ * Returns a function that transform a data's array into an object. This function
+ * was made to separate logic from data retrieving to test it.
+ * 
+ * @param {function} howToappend is a function that explain what to do with the 
+ * data processed and should have 1 parameter.
+ * @returns {function} a function that transform a data's array into an object
+ */
 var callbackRedmineRequest = function(howToappend) {
     return function(response, status) {
         if (status === "success") {
             redmineCheck = Date.now();
-            $(redmineTable).empty();
+            $(REDMINE_TABLE).empty();
             var issues = response.issues;
-            for (var i = 0; i < issues.length; i++) {
+            issues.forEach(function(arrayElement) {
                 var issue = {
-                    nameDeveloper: issues[i].assigned_to.name,
-                    subject: issues[i].subject,
-                    project: issues[i].project.name,
-                    priority: issues[i].priority.id
+                    nameDeveloper: arrayElement.assigned_to.name,
+                    subject: arrayElement.subject,
+                    project: arrayElement.project.name,
+                    priority: arrayElement.priority.id
                 };
                 howToappend(issue);
-            }
+            });
         } else {
             return status;
         }
     };
 };
 
+/**
+ * This function is used to append the properties of an object into an html table.
+ * It calls appendRow or appendFirstRow if the object is the first or not of the specified
+ * project.
+ * 
+ * @param {object} issue: the object to append
+ */
 var appendRedmineToHtml = function(issue) {
     var projectNoSpace = issue.project.replace(/ /g, "_");
     if ($.find("#" + projectNoSpace).length !== 0) {
@@ -50,29 +69,35 @@ var appendRedmineToHtml = function(issue) {
 };
 
 var appendRow = function(projectNoSpace, issue) {
-    $("#" + projectNoSpace).after("<tr><td></td><td>" + issue.subject + " " +colorPriority(issue.priority)+"</td><td>"
+    $("#" + projectNoSpace).after("<tr><td></td><td>" + issue.subject + " " + colorPriority(issue.priority) + "</td><td>"
             + issue.nameDeveloper + "</td></tr>");
 };
 
 var appendFirstRow = function(projectNoSpace, firstIssue) {
-    $(redmineTable).append("<tr id=\"" + projectNoSpace + "\"><td><strong>"
+    $(REDMINE_TABLE).append("<tr id=\"" + projectNoSpace + "\"><td><strong>"
             + firstIssue.project + "</strong></td><td>"
-            + firstIssue.subject + " " +colorPriority(firstIssue.priority) + "</td><td>"
+            + firstIssue.subject + " " + colorPriority(firstIssue.priority) + "</td><td>"
             + firstIssue.nameDeveloper + "</td></tr>");
 };
 
+/**
+ * This function evaluate the parameter and return a lable to append into html.
+ * 
+ * @param {number} num the parameter to evaluate.
+ * @returns {String} a string that should be used to append a label to html.
+ */
 function colorPriority(num) {
     switch (num) {
-        case eval(lowPriority) :
+        case eval(LOW_PRIORITY) :
             return "<span class =\"label label-info\">Bassa priorità</span>";
             break;
-        case eval(normalPriority) :
+        case eval(NORMAL_PRIORITY) :
             return "";
             break;
-        case eval(urgentPriority) :
+        case eval(URGENT_PRIORITY) :
             return "<span class =\"label label-warning\">Urgente</span>";
             break;
-        case eval(immediatePriority) :
+        case eval(IMMEDIATE_PRIORITY) :
             return "<span class =\"label label-important\">Immediata</span>";
             break;
     }
