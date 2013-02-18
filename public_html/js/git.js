c22e53596fbc5eeb580ad1c48d8d790a700c82b0
Matteo Bonetto <bonetto.matteo@anteash.com>
2013-02-18 10:24:30 +0100

    Aggiunti commenti al codice di git.js

--- public_html/js/git.js
+++ public_html/js/git.js
@@ -4,80 +4,77 @@
  */
 
 var gitCheck = (new Date()).getTime();
-var branchAppend = "#branchDiv";
-var commitAppend = "#commitDiv";
 
 getGit = function(urlCommit, urlBranch) {
-    $.get(urlCommit, fetchGit(parseCommit, printCommit));
-    $.get(urlBranch, fetchGit(parseBranch, printBranch));
-};
+    var branchAppend = "#branchDiv"; //Variabile che memorizza il div destinato alla stampa dei branch
+    var commitAppend = "#commitDiv"; //Variabile che memorizza il div destinato alla stampa dei commit
 
-var printBranch = function(parsedData) {
-    $(branchAppend).empty();
-    $(branchAppend).append("<h4>Branch più recenti</h4>");
-    for (i = 0; i < parsedData.length; i++) {
-        $(branchAppend).append("<blockquote><p>" + parsedData[i].name.text() + "</p><p><small>" + parsedData[i].date.text() + "</p></small></blockquote>");
-    }
-};
-
-var parseBranch = function(data, printFunction) {
-    var found = [];
-    data.find("tr").each(function(i) {
-        if (i === 5) {
-            return false;
-        }
-        found[i] = {};
-        $this = $(this);
-        $this.find("td").each(function(j) {
-            var $this = $(this);
-            if (j === 0) {
-                found[i].date = $this;
-                found[i].date.text(found[i].date.text().replace("months", "mesi")
-                        .replace("month", "mese").replace("weeks", "settimane")
-                        .replace("week", "settimana").replace("hours", "ore")
-                        .replace("hour", "ora").replace("ago", "fa"));
-            }
-            else if (j === 1) {
-                found[i].name = $this;
-                return false;
-            }
-        });
-    });
-    printFunction(found);
-};
-
-var printCommit = function(parsedData) {
-    $(commitAppend).html("<h4>Commit più recenti in develop</h4>");
-    for (i = 0; i < parsedData.length; i++) {
-        $(commitAppend).append("<blockquote><p>" + parsedData[i].title + "</p>" + "<p><small>" +
-                parsedData[i].author + " il " + parsedData[i].pubDate.toLocaleString() + ".</p></small></blockquote>");
-    }
-};
-
-var parseCommit = function(data, printFunction) {
-    var parsedData = [];
-    data.find("item").each(function(index) {
-        if (index === 5) {
-            return false;
-        }
-        var $this = $(this);
-        parsedData[index] = {
-            title: $this.find("title").text(),
-            pubDate: new Date($this.find("pubDate").text()),
-            author: $this.find("author").text().split(" <")[0]
-        };
-    });
-    printFunction(parsedData);
-};
-
-var fetchGit = function(parseFunction, printFunction) {
-    return function(data, textStatus) {
-        if (textStatus !== "success") {
-            return textStatus;
-        } else {
-            gitCheck = (new Date()).getTime();
-            $data = $(data);
-            parseFunction($data, printFunction);
+    var printBranch = function(parsedData) {
+        $(branchAppend).html("<h4>Branch più recenti</h4>");
+        for (i = 0; i < parsedData.length; i++) {
+            $(branchAppend).append("<blockquote><p>" + parsedData[i].name.text() + "</p><p><small>" + parsedData[i].date.text() + "</p></small></blockquote>");
         }
     };
+
+    var printCommit = function(parsedData) {
+        $(commitAppend).html("<h4>Commit più recenti in develop</h4>");
+        for (i = 0; i < parsedData.length; i++) {
+            $(commitAppend).append("<blockquote><p>" + parsedData[i].title + "</p>" + "<p><small>" +
+                    parsedData[i].author + " il " + parsedData[i].pubDate.toLocaleString() + ".</p></small></blockquote>");
+        }
+    };
+
+    var parseBranch = function(data, printFunction) {
+        var MAX_BRANCH_COUNT = 5; //Variabile che memorizza il numero massimo di branch da stampare
+        var found = []; //Array da popolare con i dati trovati, da passare alla funzione di stampa
+        data.find("tr").each(function(i) {
+            if (i === MAX_BRANCH_COUNT) {
+                return false;
+            }
+            $this = $(this);
+            found[i] = {
+                name: $this.children("td:nth-of-type(2)"), //Seconda cella della tabella dove è scritto il nome del branch
+                date: $this.children("td:nth-of-type(1)") //Prima cella della tabella dove è scritta la data di aggiornamento del branch
+            };
+            //Traduce la data da inglese ad italiano
+            found[i].date.text(found[i].date.text().replace("months", "mesi")
+                    .replace("month", "mese").replace("weeks", "settimane")
+                    .replace("week", "settimana").replace("days", "giorni")
+                    .replace("day", "giorno").replace("hours", "ore")
+                    .replace("hour", "ora").replace("ago", "fa"));
+        });
+        printFunction(found);
+    };
+
+    var parseCommit = function(data, printFunction) {
+        var MAX_COMMIT_COUNT = 5; //Variabile che memorizza il numero massimo di commit da stampare
+        var found = []; //Array da popolare con i dati trovati, da passare alla funzione di stampa
+        data.find("item").each(function(i) {
+            if (i === MAX_COMMIT_COUNT) {
+                return false;
+            }
+            var $this = $(this);
+            found[i] = {
+                title: $this.find("title").text(),
+                pubDate: new Date($this.find("pubDate").text()),
+                author: $this.find("author").text().split(" <")[0]
+            };
+        });
+        printFunction(found);
+    };
+
+    var fetchGit = function(parseFunction, printFunction) {
+        return function(data, textStatus) {
+            if (textStatus !== "success") {
+                return textStatus;
+            } else {
+                gitCheck = (new Date()).getTime();
+                $data = $(data);
+                parseFunction($data, printFunction);
+            }
+        };
+    };
+
+    $.get(urlCommit, fetchGit(parseCommit, printCommit));
+    $.get(urlBranch, fetchGit(parseBranch, printBranch));
 };
